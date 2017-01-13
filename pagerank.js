026059fe7e98aa4b6ca954a3a0f969fda024c693
Element118 <Element118@users.noreply.github.com>
2017-01-13 18:21:45 +0800

    Update pagerank.js

--- pagerank.js
+++ pagerank.js
@@ -1,18 +1,46 @@
 var fs = require("fs");
 /**
-This is a module for making a PageRank system for recommendations on Discord.
-**/
+ * This is a module for making a PageRank system for recommendations on Discord.
+ */
 module.exports = {
     name: "PageRankRecommend", // name of module
     commands: [],
     initialise: null, // or initialize. Might include file I/O
     close: null // to end. Might include file I/O. May return Promise.
 };
+/**
+ * Standard file name system used for this module.
+ */
 var getFileName = function(server, category) {
     return "PageRankRecommendFile"+server+""+category+".txt";
 };
 
+/*
+TODO:
+1. New users.
+2. Documentation.
+3. Efficiency.
+4. Actually giving roles.
+*/
+/*
+var Priority_Queue = function(init, cmpre) {
+    this.data = init || [null];
+    this.compare = cmpre || function(a, b) {
+        return a<b;
+    };
+};
+Priority_Queue.prototype.fixHeapAt(where) {
+    if (where<=0 ||)
+};
+*/
+
+/**
+ * Damping factor used for PageRank.
+ */
 var dampingFactor = 0.85;
+/**
+ * Uses PageRank to recommend users.
+ */
 var PageRank = function() {
     this.server = null;
     this.category = null;
@@ -21,13 +49,24 @@ var PageRank = function() {
     this.userIdMap = null;
     this.ranks = [];
     this.adjList = [];
+    this.roleName = "";
+    this.threshold = function(totalMembers) {
+        return 2/totalMembers;
+    };
 };
+
+/**
+ * Generates a map from the Discord User ID to the number used in this object.
+ */
 PageRank.prototype.generateUserIdMap = function() {
     this.userIdMap = {};
     for (var i=0;i<this.users.length;i++) {
         this.userIdMap[this.users[i]] = i;
     }
 };
+/**
+ * Iterates to improve accuracy of ranks, by default 10 times.
+ */
 PageRank.prototype.iterate = function(iterations) {
     iterations = iterations || 1;
     var N = this.users.length;
@@ -51,8 +90,14 @@ PageRank.prototype.iterate = function(iterations) {
         this.ranks = newRanks;
     }
 };
+/**
+ * Tries to find a file corresponding with the server and category.
+ * On failure to find file, create data.
+ * @return Promise
+ */
 PageRank.prototype.parseFile = function(server, category) {
     console.log("Getting data for "+server.name+" in category "+category);
+    if (category.startsWith("\"")) return new Promise(function(resolve, reject) { reject(); });
     var fileName = getFileName((this.server = server).name, this.category = category);
     this.userNames = [];
     this.users = [];
@@ -78,11 +123,10 @@ PageRank.prototype.parseFile = function(server, category) {
                     _this.generateUserIdMap();
                     resolve();
                     return;
-                } else {
-                    console.log(err);
-                    reject();
-                    return;
                 }
+                console.log(err);
+                reject();
+                return;
             }
             // check file does not exist, if fail, create file data
             data = data.split("\n");
@@ -96,8 +140,7 @@ PageRank.prototype.parseFile = function(server, category) {
                     _this.userNames.push(user.username+"#"+user.discriminator);
                     _this.ranks.push(+curUser[1]);
                     curUser = curUser.slice(2);
-                    console.log(curUser.length);
-                    for (var j=0;j<_this.adjList.length;j++) {
+                    for (var j=0;j<curUser.length;j++) {
                         console.log(curUser[j]);
                         curUser[j] = +curUser[j];
                     }
@@ -109,6 +152,55 @@ PageRank.prototype.parseFile = function(server, category) {
         });
     });
 };
+/**
+ * Tries to find a file corresponding with the server and category.
+ * On failure to find file, fail.
+ * @return Promise
+ */
+PageRank.prototype.parseFileSafe = function(server, category) {
+    console.log("Getting data for "+server.name+" in category "+category);
+    if (category.startsWith("\"")) return new Promise(function(resolve, reject) { reject(); });
+    var fileName = getFileName((this.server = server).name, this.category = category);
+    this.userNames = [];
+    this.users = [];
+    this.ranks = [];
+    this.adjList = [];
+    this.userIdMap = null;
+    var _this = this;
+    return new Promise(function(resolve, reject) {
+        fs.readFile(__dirname + "/" + fileName, "utf8", function(err, data) {
+            if (err) {
+                reject();
+                return;
+            }
+            // check file does not exist, if fail, create file data
+            data = data.split("\n");
+            var N = data.length;
+            var members = _this.server.members;
+            for (var i=0;i<N;i++) {
+                if (data[i]) {
+                    var curUser = data[i].split(" ");
+                    _this.users.push(curUser[0]);
+                    var user = members.get(curUser[0]).user;
+                    _this.userNames.push(user.username+"#"+user.discriminator);
+                    _this.ranks.push(+curUser[1]);
+                    curUser = curUser.slice(2);
+                    for (var j=0;j<curUser.length;j++) {
+                        console.log(curUser[j]);
+                        curUser[j] = +curUser[j];
+                    }
+                    _this.adjList.push(curUser);
+                }
+            }
+            _this.generateUserIdMap();
+            resolve();
+        });
+    });
+};
+/**
+ * Tries to save file.
+ * @return Promise
+ */
 PageRank.prototype.save = function() {
     var fileName = getFileName(this.server.name, this.category);
     var memoryString = "";
@@ -131,7 +223,9 @@ PageRank.prototype.save = function() {
         });
     });
 };
-var servers = {}; // map from server to data
+/**
+ * Using an object to store the PageRank objects in.
+ */
 var pageRanks = {}; // map from (server, category) to PageRank
 
 // http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
@@ -142,7 +236,13 @@ var unique = function(a) {
     });
 };
 
+/**
+ * Client: Your standard Discord Client.
+ */
 var Client = null;
+/**
+ * Standard sending functions.
+ */
 var send = function(message, toSend) {
     message.channel.sendMessage(toSend).then(function() {
         console.log("Message sent.");
@@ -157,8 +257,11 @@ var sendDM = function(message, toSend) {
         console.log("Failed to send DM.");
     });
 };
+/**
+ * New commands
+ */
 module.exports.commands.push({
-    word: "recommend", // command word
+    word: "recommend",
     description: "Recommend someone in a category. Use: ~M~recommend some_category username0#discriminator0 username1#discriminator1 username2#discriminator2 ...",
     execute: function(message, parsedMessage) {
         var guild = message.guild;
@@ -169,7 +272,7 @@ module.exports.commands.push({
         var pageRank = pageRanks[getFileName(guild.name, category)];
         if (!pageRank) {
             pageRank = pageRanks[getFileName(guild.name, category)] = new PageRank();
-            pageRank.parseFile(guild, category).then(function() {
+            pageRank.parseFileSafe(guild, category).then(function() {
                 // do the update
                 user = pageRank.userIdMap[user];
                 for (var i=1;i<tokens.length;i++) {
@@ -201,7 +304,7 @@ module.exports.commands.push({
         }
     }
 }, {
-    word: "unrecommend", // command word
+    word: "unrecommend",
     description: "Unrecommend someone in a category. Use: ~M~unrecommend some_category username0#discriminator0 username1#discriminator1 username2#discriminator2 ...",
     execute: function(message, parsedMessage) {
         var guild = message.guild;
@@ -212,7 +315,7 @@ module.exports.commands.push({
         var pageRank = pageRanks[getFileName(guild.name, category)];
         if (!pageRank) {
             pageRank = pageRanks[getFileName(guild.name, category)] = new PageRank();
-            pageRank.parseFile(guild, category).then(function() {
+            pageRank.parseFileSafe(guild, category).then(function() {
                 // do the update
                 user = pageRank.userIdMap[user];
                 for (var i=1;i<tokens.length;i++) {
@@ -238,7 +341,7 @@ module.exports.commands.push({
         }
     }
 }, {
-    word: "viewrecommends", // command word
+    word: "viewrecommends",
     description: "View your recommendations in a category. Use: ~M~viewrecommends some_category",
     execute: function(message, parsedMessage) {
         var guild = message.guild;
@@ -249,7 +352,7 @@ module.exports.commands.push({
         var pageRank = pageRanks[getFileName(guild.name, category)];
         if (!pageRank) {
             pageRank = pageRanks[getFileName(guild.name, category)] = new PageRank();
-            pageRank.parseFile(guild, category).then(function() {
+            pageRank.parseFileSafe(guild, category).then(function() {
                 user = pageRank.userIdMap[user];
                 var recommendations = "";
                 for (var i=0;i<pageRank.adjList[user].length;i++) {
@@ -270,7 +373,7 @@ module.exports.commands.push({
         }
     }
 }, {
-    word: "addcategory", // command word
+    word: "addcategory",
     description: "[MODERATOR] Add a category. Use: ~M~addcategory some_category",
     execute: function(message, parsedMessage) {
         var roles = message.member.roles;
@@ -287,7 +390,7 @@ module.exports.commands.push({
         }
     }
 }, {
-    word: "deletecategory", // command word
+    word: "deletecategory",
     description: "[MODERATOR] Delete a category. Use: ~M~deletecategory some_category",
     execute: function(message, parsedMessage) {
         var roles = message.member.roles;
@@ -296,23 +399,82 @@ module.exports.commands.push({
         }
     }
 }/*, {
-    word: "viewcategories", // command word
+    word: "viewcategories",
     description: "View all categories on the current server. Use: ~M~viewcategories",
     execute: function(message, parsedMessage) {
         //
         send(message, "Hi! I'm from another module!");
     }
-}, {
-    word: "viewcategory", // command word
-    description: "View the top users in a particular category. Use: ~M~viewcategory some_category",
+}*/, {
+    word: "viewcategory",
+    description: "View the top users in a particular category. Use: ~M~viewcategory some_category num_users",
     execute: function(message, parsedMessage) {
-        //
-        send(message, "Hi! I'm from another module!");
+        var guild = message.guild;
+        var tokens = parsedMessage.split(" ");
+        var category = tokens[0];
+        var numUsers = tokens[1] || 10; // default: 10
+        var user = message.author.id;
+        // check if the pageRank has already existed
+        var pageRank = pageRanks[getFileName(guild.name, category)];
+        if (!pageRank) {
+            pageRank = pageRanks[getFileName(guild.name, category)] = new PageRank();
+            pageRank.parseFile(guild, category).then(function() {
+                pageRank.iterate(10); // to be safe
+                console.log("Done iterations.");
+                var selectedUsers = [];
+                for (var i=0;i<pageRank.users.length;i++) {
+                    if (selectedUsers.length < numUsers) {
+                        selectedUsers.push(i);
+                    } else {
+                        // TODO: Increase efficiency later
+                        selectedUsers.sort(function(a, b) {
+                            return pageRank.ranks[a]-pageRank.ranks[b];
+                        });
+                        if (pageRank.ranks[selectedUsers[0]] < pageRank.ranks[i]) {
+                            selectedUsers[0] = i;
+                        }
+                    }
+                }
+                selectedUsers.sort(function(a, b) {
+                    return pageRank.ranks[a]-pageRank.ranks[b];
+                });
+                result = "Here are the top "+numUsers+" in category \""+category+"\" in the server \""+guild.name+"\"";
+                for (var i=selectedUsers.length-1;i>=0;i--) {
+                    result += "\n" + pageRank.ranks[selectedUsers[i]].toFixed(3) + "\t" + pageRank.userNames[selectedUsers[i]] 
+                }
+                sendDM(message, result);
+            }).catch(function() {
+                console.log("Failed to view top users.");
+            });
+        } else {
+            pageRank.iterate(10); // to be safe
+            console.log("Done iterations.");
+            var selectedUsers = [];
+            for (var i=0;i<pageRank.users.length;i++) {
+                if (selectedUsers.length < numUsers) {
+                    selectedUsers.push(i);
+                } else {
+                    // TODO: Increase efficiency later
+                    selectedUsers.sort(function(a, b) {
+                        return pageRank.ranks[a]-pageRank.ranks[b];
+                    });
+                    if (pageRank.ranks[selectedUsers[0]] < pageRank.ranks[i]) {
+                        selectedUsers[0] = i;
+                    }
+                }
+            }
+            selectedUsers.sort(function(a, b) {
+                return pageRank.ranks[a]-pageRank.ranks[b];
+            });
+            result = "Here are the top "+numUsers+" in category \""+category+"\" in the server \""+guild.name+"\"";
+            for (var i=selectedUsers.length-1;i>=0;i--) {
+                result += "\n" + pageRank.ranks[selectedUsers[i]].toFixed(3) + "\t" + pageRank.userNames[selectedUsers[i]] 
+            }
+            sendDM(message, result);
+        }
     }
-}*/);
-/**
-Client: Your standard Discord Client.
-*/
+});
+
 module.exports.initialise = function(client) {
     Client = client;
     console.log("Initialised!");
